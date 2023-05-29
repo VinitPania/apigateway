90b454f7edac5f08b90e8b01c75a1cac9029fd9d
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-29 10:55:04 +0530

    changed the port no  to 80 instead of 8999

--- src/main/java/com/apigateway/ApigatewayApplication.java
+++ src/main/java/com/apigateway/ApigatewayApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ApigatewayApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
